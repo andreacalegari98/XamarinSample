f4bbee3196bb1d82c0d87d00901117d0122da6ab
andrea.calegari <andrea.calegari@Mac-mini.local>
2017-10-18 16:34:07 +0200

    Bugfix

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -4186,6 +4186,9 @@ namespace XamarinSample.Droid
 			// aapt resource value: 0x7f080039
 			public const int abc_toolbar_collapse_description = 2131230777;
 			
+			// aapt resource value: 0x7f080056
+			public const int apiKey = 2131230806;
+			
 			// aapt resource value: 0x7f080048
 			public const int appbar_scrolling_view_behavior = 2131230792;
 			
